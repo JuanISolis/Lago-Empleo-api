f2b2db636890826d9a52a2bf1513a68ceff4a1eb
JuanISolis <juan1999bone@gmail.com>
2025-06-03 14:16:48 -0500

    creacion de metodos en la interface y la clase usuario

---
+++ app/Arquitectura/Clases/Usuario.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Arquitectura\Clases;
+
+use App\Arquitectura\Interfaces;
+use App\Models\Usuario;
+
+class UsuarioClase implements MercadoLaboral{
+    
+    public function obtenerTodos()
+    {
+        return Libro::all();
+    }
+
+    public function crear(array $datos)
+    {
+        return Libro::create($datos);
+    }
+
+    public function show(int $id)
+    {
+        return Libro::create($datos);
+    }
+
+    public function actualizar(array $datos)
+    {
+        return Libro::create($datos);
+    }
+
+}
