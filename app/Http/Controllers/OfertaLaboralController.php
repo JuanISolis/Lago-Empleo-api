b99a0f7c4da8551267de81bd83901d89474ebd4a
SMMONSTERS <sarmientomarlon77@gmail.com>
2025-09-23 19:00:37 -0500

    Actualizacion nueva 2

--- app/Http/Controllers/OfertaLaboralController.php
+++ app/Http/Controllers/OfertaLaboralController.php
@@ -8,9 +8,7 @@ use Illuminate\Routing\Controller;
 
 class OfertaLaboralController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     */
+   
     public function index()
     {
         $ofertas = OfertaLaboral::all();
