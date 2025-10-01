65ead208fb244852e81a22c6678ff58719d5be59
Janeth-Guanulema <janeth.guanulema@gmail.com>
2025-10-01 11:41:31 -0500

    completo metodo get habilidad

--- app/Models/LibreriaHabilidad.php
+++ app/Models/LibreriaHabilidad.php
@@ -6,12 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class LibreriaHabilidad extends Model
 {
+
+    protected $table = 'libreria_habilidads';
+
     protected $fillable = [
         'habilidad',
     ];
     
     public function habilidades()
     {
-        return $this->hasMany(Habilidad::class);
+        return $this->hasMany(Habilidad::class, 'libreria_habilidades_id');
     }
 }
